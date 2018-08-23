d85df612bc503c79fe5dd767f96a3ac421afe7b4
zhichenyao <293584243@qq.com>
2018-08-23 18:13:47 +0800

    listfunction

--- Music/app/src/main/java/com/example/mrxie/music/fragment/songListFragment.java
+++ Music/app/src/main/java/com/example/mrxie/music/fragment/songListFragment.java
@@ -1,21 +1,118 @@
 package com.example.mrxie.music.fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.mrxie.music.Adapter.TabFragmentPagerAdapter;
 import com.example.mrxie.music.R;
 
+import java.util.ArrayList;
+import java.util.List;
 
-public class songListFragment extends Fragment {
+public class songListFragment extends Fragment implements View.OnClickListener {
+    private TextView tv_item_one;
+    private TextView tv_item_two;
+    private TextView tv_item_three;
+    private ViewPager myViewPager;
+    private List<Fragment> list;
+    private TabFragmentPagerAdapter adapter;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-      View view=inflater.inflate(R.layout.songlist,container,false);
+        View view=inflater.inflate(R.layout.songlist,container,false);
+        tv_item_one = (TextView) view.findViewById(R.id.tv_item_one);
+        tv_item_two = (TextView) view.findViewById(R.id.tv_item_two);
+        tv_item_three = (TextView) view.findViewById(R.id.tv_item_three);
+        myViewPager = (ViewPager) view.findViewById(R.id.myViewPager);
+        tv_item_one.setOnClickListener(this);
+        tv_item_two.setOnClickListener(this);
+        tv_item_three.setOnClickListener(this);
+        myViewPager.setOnPageChangeListener(new MyPagerChangeListener());
+        list = new ArrayList<>();
+        list.add(new OneFragment());
+        list.add(new TwoFragment());
+        list.add(new ThreeFragment());
+        adapter = new TabFragmentPagerAdapter(getFragmentManager(), list);
+        myViewPager.setAdapter(adapter);
+        myViewPager.setCurrentItem(0);  //初始化显示第一个页面
+        tv_item_one.setBackgroundColor(Color.RED);//被选中就为红色
         return view;
+    }
+
+
+
+
+
+    /**
+     * 点击事件
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tv_item_one:
+                myViewPager.setCurrentItem(0);
+                tv_item_one.setBackgroundColor(Color.RED);
+                tv_item_two.setBackgroundColor(Color.WHITE);
+                tv_item_three.setBackgroundColor(Color.WHITE);
+                break;
+            case R.id.tv_item_two:
+                myViewPager.setCurrentItem(1);
+                tv_item_one.setBackgroundColor(Color.WHITE);
+                tv_item_two.setBackgroundColor(Color.RED);
+                tv_item_three.setBackgroundColor(Color.WHITE);
+                break;
+            case R.id.tv_item_three:
+                myViewPager.setCurrentItem(2);
+                tv_item_one.setBackgroundColor(Color.WHITE);
+                tv_item_two.setBackgroundColor(Color.WHITE);
+                tv_item_three.setBackgroundColor(Color.RED);
+                break;
+        }
+    }
+
+    /**
+     * 设置一个ViewPager的侦听事件，当左右滑动ViewPager时菜单栏被选中状态跟着改变
+     *
+     */
+    public class MyPagerChangeListener implements ViewPager.OnPageChangeListener {
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+        }
+
+        @Override
+        public void onPageSelected(int arg0) {
+            switch (arg0) {
+                case 0:
+                    tv_item_one.setBackgroundColor(Color.RED);
+                    tv_item_two.setBackgroundColor(Color.WHITE);
+                    tv_item_three.setBackgroundColor(Color.WHITE);
+                    break;
+                case 1:
+                    tv_item_one.setBackgroundColor(Color.WHITE);
+                    tv_item_two.setBackgroundColor(Color.RED);
+                    tv_item_three.setBackgroundColor(Color.WHITE);
+                    break;
+                case 2:
+                    tv_item_one.setBackgroundColor(Color.WHITE);
+                    tv_item_two.setBackgroundColor(Color.WHITE);
+                    tv_item_three.setBackgroundColor(Color.RED);
+                    break;
+            }
+        }
     }
 }
